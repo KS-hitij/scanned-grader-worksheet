f12d90a5ce222d17e82727a8af6534f52f832c83
KS-hitij <kshitijmamgain@jmangroup.com>
2026-04-24 15:08:32 +0530

    Feat excluded packages which lazy load

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["chromadb", "@chroma-core/google-gemini", "pdf-parse"],
+  serverExternalPackages: ["langchain",
+    "@langchain/core",
+    "@langchain/langgraph",
+    "@langchain/google-genai",
+    "@langchain/community",
+    "chromadb",
+    "@chroma-core/google-gemini",
+    "pdf-parse",
+    "@google/genai",],
 };
 
 export default nextConfig;
